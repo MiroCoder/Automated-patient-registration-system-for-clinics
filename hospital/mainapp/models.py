5437fa02b6a244733e001a3b6431f6006d9f9b58
Miroslav <138483137+MiroCoder@users.noreply.github.com>
2025-02-20 11:53:20 +0100

    English complete version

--- hospital/mainapp/models.py
+++ hospital/mainapp/models.py
@@ -9,22 +9,22 @@ import datetime
 class ProfileModel(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True, verbose_name='Адрес', null=True)
-    birth_date = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
+    location = models.CharField(max_length=30, blank=True, verbose_name='Address', null=True)
+    birth_date = models.DateField(null=True, blank=True, verbose_name='Date of birth')
 
     GENDER_CHOICES = (
-        ('M', 'Мужской'),
-        ('F', 'Женский'),
+        ('M', 'Male'),
+        ('F', 'Female'),
     )
 
-    full_name = models.CharField(max_length=255, verbose_name='ФИО')
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name='Пол')
-    contact_number = models.CharField(max_length=20, verbose_name='Контактный номер', blank=True,null=True)
+    full_name = models.CharField(max_length=255, verbose_name='Name and Surname')
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name='Sex')
+    contact_number = models.CharField(max_length=20, verbose_name='Contact number', blank=True,null=True)
 
 
     class Meta:
-        verbose_name = 'Пациент'
-        verbose_name_plural = 'Пациенты'
+        verbose_name = 'Patient'
+        verbose_name_plural = 'Patients'
 
     def __str__(self):
         return self.full_name
@@ -38,11 +38,11 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 class DoctorModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    name = models.CharField(max_length=100, verbose_name="ФИО")
-    specialty = models.CharField(max_length=100, verbose_name="Специальность")
+    name = models.CharField(max_length=100, verbose_name="Full name")
+    specialty = models.CharField(max_length=100, verbose_name="Specialization")
     class Meta:
-        verbose_name = 'Доктор'
-        verbose_name_plural = 'Доктора'
+        verbose_name = 'Doctor'
+        verbose_name_plural = 'Doctors'
 
     def __str__(self):
         return f"{self.name} ({self.specialty})"
@@ -57,8 +57,8 @@ class AppointmentModel(models.Model):
     doctor = models.ForeignKey(DoctorModel, on_delete=models.SET_NULL, blank=True,null=True)
 
     class Meta:
-        verbose_name = 'Запись'
-        verbose_name_plural = 'Записи'
+        verbose_name = 'Appointment'
+        verbose_name_plural = 'Appointments'
 
     def __str__(self):
         return str(self.__dict__)
@@ -73,35 +73,35 @@ class VisitModel(models.Model):
     reason = models.TextField()
 
     class Meta:
-        verbose_name = 'Запись'
-        verbose_name_plural = 'Записи'
+        verbose_name = 'Appointment'
+        verbose_name_plural = 'Appointments'
 
     def __str__(self):
         return f"{self.visit_date} - {self.patient} - {self.doctor} - {self.reason}"
 
 class ScheduleModel(models.Model):
     doctor = models.ManyToManyField(DoctorModel, blank=True)
-    day_of_week = models.CharField(max_length=20, default='Понедельник')
+    day_of_week = models.CharField(max_length=20, default='Monday')
     start_time = models.TimeField()
     end_time = models.TimeField()
 
     class Meta:
-        verbose_name = 'Расписание'
-        verbose_name_plural = 'Расписание'
+        verbose_name = 'Schedule'
+        verbose_name_plural = 'Schedule'
 
     def __str__(self):
         return f"{self.doctor} - {self.day_of_week} ({self.start_time} - {self.end_time})"
 
 class MedicalRecordModel(models.Model):
-    patient = models.ForeignKey(ProfileModel, on_delete=models.CASCADE, verbose_name='Пациент')
-    diagnosis = models.TextField(verbose_name='Диагноз')
-    prescription = models.TextField(verbose_name='Рецепт')
-    creation_date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
-    last_update_date = models.DateField(auto_now=True, verbose_name='Дата последнего обновления')
+    patient = models.ForeignKey(ProfileModel, on_delete=models.CASCADE, verbose_name='Patient')
+    diagnosis = models.TextField(verbose_name='Diagnosis')
+    prescription = models.TextField(verbose_name='Receipts')
+    creation_date = models.DateField(auto_now_add=True, verbose_name='Creation date')
+    last_update_date = models.DateField(auto_now=True, verbose_name='Last date refreshing')
 
 
     class Meta:
-        verbose_name = 'Медицинская карта'
-        verbose_name_plural = 'Медицинские карты'
+        verbose_name = 'Medical card'
+        verbose_name_plural = 'Medical cards'
     def __str__(self):
         return str(self.__dict__)
